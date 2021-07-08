6b076a95d6d6422426517c4eded8e866fd41c99d
grawish <grawish06@gmail.com>
2021-07-09 01:02:47 +0530

    Add File Manager and drag and drop

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let desktopicons=document.getElementsByClassName("desktop-icon");
+let trashBtn = document.getElementById("trash");
+let browserBtn = document.getElementById("browser");
+let homeBtn = document.getElementById("home");
+let fmX = 0;
+let fmY = 0;
+let fm = document.getElementById("file-manager");
+let maxBtn = document.querySelectorAll('.max-btn');
+
+homeBtn.addEventListener('click',function (){
+    fm.classList.add("active");
+});
+maxBtn.forEach((event)=>{
+    console.log(event);
+})
 
 
-for (let desktopicon of desktopicons) {
-    desktopicon.addEventListener("click",(event) => {
-        event.target.classList.toggle("selected");
-    });
+fm.ondragstart = (event) => {
+    fmX = "" + (event.pageX - fm.offsetLeft);
+    fmY = "" + (event.pageY - fm.offsetTop);
+    fm.style.opacity = "0.01";
+}
+fm.ondragend = (event) => {
+    fm.style.left = "" + (event.clientX - fmX) + "px";
+    fm.style.top = "" + (event.clientY - fmY) + "px";
+    fm.style.opacity = "1";
 }
