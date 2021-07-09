8e783df3becf741da5dbb363580ada2263253f7b
grawish <grawish06@gmail.com>
2021-07-10 01:10:23 +0530

    Add browser and powermenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 let trashBtn = document.getElementById("trash");
-let browserBtn = document.getElementById("browser");
+let browserBtn = document.getElementById("chrome");
 let homeBtn = document.getElementById("home");
 let fmX = 0;
 let fmY = 0;
 let fm = document.getElementById("file-manager");
-let maxBtn = document.querySelectorAll('.max-btn');
-let titleBar=document.querySelector('.titlebar');
+let titleBar = document.querySelector('.titlebar');
+let browser = document.getElementById('browser');
+let iframe = document.getElementById('browser-window');
+let powerBtn = document.getElementById('shutdown');
+let powerMenu = document.getElementById('powermenu');
 
 homeBtn.addEventListener('click', function () {
     fm.classList.add("active");
 });
-
+browserBtn.addEventListener('click', function () {
+    browser.classList.add('active');
+});
+powerBtn.addEventListener('click', function () {
+    powerMenu.classList.add('active');
+});
 
 fm.ondragstart = (event) => {
     fmX = "" + (event.pageX - fm.offsetLeft);
@@ -22,30 +30,51 @@ fm.ondragend = (event) => {
     fm.style.top = "" + (event.clientY - fmY) + "px";
     fm.style.opacity = "1";
 }
-titleBar.addEventListener('mousedown',()=>{
+titleBar.addEventListener('mousedown', () => {
     titleBar.parentNode.draggable = true;
 })
-titleBar.addEventListener('mouseup',()=>{
+titleBar.addEventListener('mouseup', () => {
     titleBar.parentNode.draggable = false;
 })
-titleBar.addEventListener('mouseleave',()=>{
+titleBar.addEventListener('mouseleave', () => {
     titleBar.parentNode.draggable = false;
 })
 
 
-function maxfm() {
-    fm.classList.toggle("maximise");
-    fm.style.top="5vh";
-    fm.style.left="0";
+function maxEle(element) {
+    element.classList.toggle("maximise");
+    element.style.top = "5vh";
+    element.style.left = "0";
 }
-function closefm() {
-    fm.classList.remove("active");
+
+function closeEle(element) {
+    element.classList.remove("active");
 }
-function maxterm() {
-    fm.classList.toggle("maximise");
-    fm.style.top="5vh";
-    fm.style.left="0";
+
+function browserBack() {
+    alert("blocked due to security issue!");
 }
-function closeterm() {
-    fm.classList.remove("active");
+
+function browserFwd() {
+    alert("blocked due to security issue!");
+}
+
+function browserHome() {
+    iframe.src = "https://google.com/webhp?igu=1";
+}
+
+function browserReload() {
+    iframe.src = iframe.src;
+}
+
+function browserUrl() {
+    iframe.src = document.getElementById('iframe-url').value;
+}
+
+function poweroff() {
+    window.close();
+}
+
+function restart() {
+    window.reload();
 }
